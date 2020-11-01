8c44c6e95ef17d6a1cfeaaf45cd2f44183a66142
Ben <bigriffin94@outlook.com>
2020-11-01 14:09:53 -0800

    Event Modifiers

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ new Vue({
         websiteTag: '<a href="https://github.com/B-Griffinn/vuejs-basics"> BOUND </a>',
         age: 26,
         x: 0,
-        y:0
+        y:0,
+        modifyMe: 0,
     },
 
     // methonds
@@ -22,15 +23,21 @@ new Vue({
         },
 
         addYear: function(increment) {
-            this.age+=increment
+            this.age += increment
         },
         subtractYear: function(decrement) {
-            this.age-=decrement
+            this.age -= decrement
         },
         // track cursor
         updateXY: function(event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        modifyFn: function(inc) {
+            this.modifyMe += inc
+        },
+        clickHref: function() {
+            alert("You clicked meh")
         }
     },
 });
